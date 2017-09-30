1adebf644dcc4b7e9c6d54cf6459a13e5efa89e9
Eric Binnion <ericbinnion@gmail.com>
2017-09-29 22:47:50 -0500

    First pass at creating a WordPress site; WIP

--- shell/provision-single-site.sh
+++ shell/provision-single-site.sh
@@ -1,36 +1,35 @@
 #!/bin/bash
 
-RED='\033[0;31m'
-PURPLE='\033[0;35m'
-BLUE='\033[1;34m'
-GREEN='\033[1;32m'
-NC='\033[0m'
+# RED='\033[0;31m'
+# PURPLE='\033[0;35m'
+# BLUE='\033[1;34m'
+# GREEN='\033[1;32m'
+# NC='\033[0m'
 
 echo "Please enter the name of the app:"
-read app_name
+read -r app_name
 
-echo "Please choose from one of the following system users:"
-serverpilot sysusers
-read sysuser
+while [ "$php_version" != "php5.4" ] && [ "$php_version" != "php5.5" ] && [ "$php_version" != "php5.6" ] && [ "$php_version" != "php7.0" ] && [ "$php_version" != "php7.1" ]; do
+	echo "Please select which version of PHP (php5.4, php5.5, php5.6, php7.0, or php7.1):"
+	read -r php_version
+done
 
-echo "Please select which version of PHP (php5.4, php5.5, php5.6, php7.0, or php7.1):"
-read php_version
-
-echo "Please enter a space and/or comma separated list of domains to use for the site:"
-read domains
+echo "Please enter a domain to use for the site:"
+read -r domain
 
 echo "What is the name of your new site?"
-read site_title
+read -r site_title
 
 echo "What is the admin username?"
-read admin_username
+read -r admin_username
 
 echo "What is the admin password?"
-read admin_password
+read -r admin_password
 
 echo "What is the admin's email address?"
-read admin_email
+read -r admin_email
 
+result=$( serverpilot apps create "$app_name" "$serverpilot_server_id" "$php_version" \
+	'["'"$domain"'"]' '{"site_title":"'"$site_title"'","admin_user":"'"$admin_username"""'","admin_password":"'"$admin_password"'","admin_email":"'"$admin_email"'"}')
 
-
-# serverpilot apps create wordpress RvnwAIfuENyjUVnl php7.0 '["example.com","www.example.com"]' '{"site_title":"My WordPress Site","admin_user":"admin","admin_password":"mypassword","admin_email":"example@example.com"}'
+echo "$result"
